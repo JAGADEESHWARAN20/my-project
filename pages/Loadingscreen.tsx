449e3f79295d931cc1c317db50bb251894c3003e
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2024-09-08 14:57:09 +0530

    framework ready

--- pages/Loadingscreen.tsx
+++ pages/Loadingscreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import{ gsap} from 'gsap';
+import { gsap } from 'gsap';
 
 const LoadingScreen: React.FC<{ onLoadingComplete: () => void }> = ({ onLoadingComplete }) => {
   const [loading, setLoading] = useState(0);
@@ -27,36 +27,36 @@ const LoadingScreen: React.FC<{ onLoadingComplete: () => void }> = ({ onLoadingC
 
     // Add text animation to the timeline
     timeline.to(textRef.current, {
-      y: -50,
+      y: 0,
       duration: 2,
       ease: 'power2.inOut',
-      onComplete: ()=>{
+      onComplete: () => {
         timeline.to(textRef.current, {
-            opacity: 0,
-            duration:1,
+          opacity: 0,
+          duration: 1,
         })
       }
     }, 0); // Start at time 0
 
     // Add loading line animation to the timeline
     timeline.to(loadingLineRef.current, {
-      width: '100%',
+      width: '80%',
       duration: 3,
       ease: 'power2.inOut',
-      onComplete:()=>{
-    timeline.to(loadingLineRef.current, {
-        opacity:0,
-    }),
-        timeline.to(loadingRef.current, {
+      onComplete: () => {
+        timeline.to(loadingLineRef.current, {
+          opacity: 0,
+        }),
+          timeline.to(loadingRef.current, {
             height: 0,
-            y: -100,
+            y: 0,
             duration: 1,
             ease: 'power2.inOut',
-            onComplete:()=>{
-                timeline.to(loadingRef.current, {
-                    opacity:0,
-                    duration:2,
-                })
+            onComplete: () => {
+              timeline.to(loadingRef.current, {
+                opacity: 0,
+                duration: 2,
+              })
             }
           });
       }
@@ -65,15 +65,15 @@ const LoadingScreen: React.FC<{ onLoadingComplete: () => void }> = ({ onLoadingC
     // Animate the container's background gradient
     const gradientAnimation = gsap.timeline({ repeat: -1, yoyo: true });
     gradientAnimation.to(loadingRef.current, {
-      background: 'radial-gradient(circle, rgba(66,130,198,1), rgba(0,7,9,1))',
-      duration: 2,
+      background: '#0000',
+      duration: 5,
       ease: '2s',
-      onComplete:()=>{
-        gradientAnimation.to(loadingRef.current, {    
-            opacity:0,
-            duration:1,
-      })
-    }
+      onComplete: () => {
+        gradientAnimation.to(loadingRef.current, {
+          opacity: 0,
+          duration: 1,
+        })
+      }
     });
 
 
@@ -105,18 +105,19 @@ const styles = {
     overflow: 'hidden',
   },
   loadingText: {
-    fontSize: '10em',
+    fontSize: '3em',
     color: 'white',
     zIndex: 10000, // Ensure it's on top of the container
   },
   loadingLine: {
     width: '0%', // Start with width 0
-    height: '10px',
+    height: '4px',
     backgroundColor: 'white',
     zIndex: 9998, // Ensure it's below the text
     marginTop: '200px',
+    left: '10%',
     position: 'absolute' as 'absolute', // Use absolute positioning to keep it at the bottom
-    left: '0',
+
   },
 };
 
